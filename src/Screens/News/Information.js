65d6f5a6b716756d2f62e542665dba4fbd71aeb7
peeoen <theerawat.pon@gmail.com>
2021-10-16 04:11:27 +0700

    upload file from texteditor to server.

--- src/Screens/News/Information.js
+++ src/Screens/News/Information.js
@@ -20,7 +20,7 @@ const drawerHeight = "100%";
 const drawerwidth = "100%";
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: drawerHeight,
+    height: drawerHeight, 
     width: drawerwidth,
     marginTop: 20,
   },
@@ -42,13 +42,121 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-ClassicEditor.create(document.querySelector("#editor"))
-  .then((editor) => {
-    editor.ui.view.editable.element.style.height = "250px";
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+
+class MyUploadAdapter {
+  constructor( loader ) {
+      // The file loader instance to use during the upload.
+      this.loader = loader;
+  }
+
+  // Starts the upload process.
+  upload() {
+      return this.loader.file
+          .then( file => new Promise( ( resolve, reject ) => {
+              this._initRequest();
+              this._initListeners( resolve, reject, file );
+              this._sendRequest( file );
+          } ) );
+  }
+
+  // Aborts the upload process.
+  abort() {
+      if ( this.xhr ) {
+          this.xhr.abort();
+      }
+  }
+
+  // Initializes the XMLHttpRequest object using the URL passed to the constructor.
+  _initRequest() {
+      const xhr = this.xhr = new XMLHttpRequest();
+
+      // Note that your request may look different. It is up to you and your editor
+      // integration to choose the right communication channel. This example uses
+      // a POST request with JSON as a data structure but your configuration
+      // could be different.
+      xhr.open( 'POST', 'https://apibridgestone.kivaru.com/api/upload/file', true );
+      xhr.setRequestHeader("Authorization", `Bearer ${localStorage.getItem('token')}`)
+      xhr.responseType = 'json';
+  }
+
+  // Initializes XMLHttpRequest listeners.
+  _initListeners( resolve, reject, file ) {
+      const xhr = this.xhr;
+      const loader = this.loader;
+      const genericErrorText = `Couldn't upload file: ${ file.name }.`;
+
+      xhr.addEventListener( 'error', () => reject( genericErrorText ) );
+      xhr.addEventListener( 'abort', () => reject() );
+      xhr.addEventListener( 'load', () => {
+          const response = xhr.response;
+
+
+          // This example assumes the XHR server's "response" object will come with
+          // an "error" which has its own "message" that can be passed to reject()
+          // in the upload promise.
+          //
+          // Your integration may handle upload errors in a different way so make sure
+          // it is done properly. The reject() function must be called when the upload fails.
+          if ( !response || response.error ) {
+              return reject( response && response.error ? response.error.message : genericErrorText );
+          }
+
+          console.log(response);
+          // If the upload is successful, resolve the upload promise with an object containing
+          // at least the "default" URL, pointing to the image on the server.
+          // This URL will be used to display the image in the content. Learn more in the
+          // UploadAdapter#upload documentation.
+          resolve( {
+              default: response?.results?.path
+          } );
+      } );
+
+      // Upload progress when it is supported. The file loader has the #uploadTotal and #uploaded
+      // properties which are used e.g. to display the upload progress bar in the editor
+      // user interface.
+      if ( xhr.upload ) {
+          xhr.upload.addEventListener( 'progress', evt => {
+              if ( evt.lengthComputable ) {
+                  loader.uploadTotal = evt.total;
+                  loader.uploaded = evt.loaded;
+              }
+          } );
+      }
+  }
+
+  // Prepares the data and sends the request.
+  _sendRequest( file ) {
+      // Prepare the form data.
+      const data = new FormData();
+
+      data.append( 'file_upload', file );
+
+      // Important note: This is the right place to implement security mechanisms
+      // like authentication and CSRF protection. For instance, you can use
+      // XMLHttpRequest.setRequestHeader() to set the request headers containing
+      // the CSRF token generated earlier by your application.
+
+      // Send the request.
+      this.xhr.send( data );
+  }
+}
+
+function MyCustomUploadAdapterPlugin( editor ) {
+  editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
+      // Configure the URL to the upload script in your back-end here!
+      return new MyUploadAdapter( loader );
+  };
+}
+
+// ClassicEditor.create(document.querySelector("#editor"), {
+//   extraPlugins: [MyCustomUploadAdapterPlugin],
+// })
+//   .then((editor) => {
+//     editor.ui.view.editable.element.style.height = "250px";
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
 const BootstrapInput = styled(InputBase)(({ theme }) => ({
   //   "label + &": {
@@ -103,7 +211,7 @@ export default function Information() {
     <div key={file.name}>
       <div>
         <img src={file.preview} style={{ width: "150px" }} alt="preview" />
-        
+
         {/* {file.name} */}
       </div>
     </div>
@@ -143,7 +251,7 @@ export default function Information() {
               <div {...getRootProps()}>
                 <input
                   {...getInputProps()}
-                  // type="file"
+                // type="file"
                 />
                 {Files.length > 0 ? (
                   <div>{images}</div>
@@ -228,6 +336,13 @@ export default function Information() {
                 id="editor"
                 editor={ClassicEditor}
                 data=""
+                config={
+                  {
+                    extraPlugins: [MyCustomUploadAdapterPlugin]
+                  }
+                }
+                // plugins={[ CKFinder]}
+              
                 onReady={(editor) => {
                   // You can store the "editor" and use when it is needed.
                   console.log("Editor is ready to use!", editor);
@@ -353,7 +468,7 @@ export default function Information() {
               <div {...getRootProps()}>
                 <input
                   {...getInputProps()}
-                  // type="file"
+                // type="file"
                 />
                 <div
                   style={{
